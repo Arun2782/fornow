af39d9ef337f031d8f404ec55a1dc29ab066b92e
Arun2782 <arunkrishnaanu@gmail.com>
2023-06-09 14:27:23 +0530

    tryingnew

---
+++ src/com/git/StringD.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class StringD {
+	
+	private void employee() {
+		// TODO Auto-generated method stub
+		System.out.println("subbu");
+
+	} private void manager() {
+		// TODO Auto-generated method stub
+System.out.println("koki");
+	} private void owned() {
+		// TODO Auto-generated method stub
+System.out.println("adhi");
+	}
+	
+	}
